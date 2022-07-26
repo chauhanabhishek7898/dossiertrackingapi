dd2cc0afeea2d6c4bbbb38a3a50a17aa1c8ebbdd
Ajit Patri <ajit_patri@hotmail.com>
2022-07-26 18:33:12 +0530

    ok

---
+++ TrackingAPI/Models/OrderDetails.cs
@@ -0,0 +1,23 @@
+﻿namespace TrackingAPI.Models
+{
+    public class OrderDetails
+    {
+        public int nTrackId { get; set; }
+        public string vTrackId { get; set; }
+        public string dtDated { get; set; }
+        public string vTime { get; set; }
+        public int nCustomerUserId { get; set; }
+        public int nCityId { get; set; }
+        public double nTotalKMs { get; set; }
+        public double nTotalRate { get; set; }
+        public double nTotalWaitingTimeInMinutes { get; set; }
+        public double nTotalWaitingPrice { get; set; }
+        public double nDiscountAny { get; set; }
+        public double nGrandTotal { get; set; }
+        public bool btActive { get; set; }
+        public bool btPaid { get; set; }
+        public string vRemarks { get; set; }
+        public int nDriverUserId { get; set; }
+        public bool btFinalClosed { get; set; }
+    }
+}
---
+++ TrackingAPI/Models/OrderRouteDetails.cs
@@ -0,0 +1,25 @@
+﻿namespace TrackingAPI.Models
+{
+    public class OrderRouteDetails
+    {
+        public int nTrackId { get; set; }
+        public int nRouteId { get; set; }
+        public string vSource { get; set; }
+        public string vSourceLat { get; set; }
+        public string vSourceLong { get; set; }
+        public string vSourceAddress { get; set; }
+        public string vDestination { get; set; }
+        public string vDestinationLat { get; set; }
+        public string vDestinationLong { get; set; }
+        public string vDestinationAddress { get; set; }
+        public int nKMs { get; set; }
+        public int nRate { get; set; }
+        public int nWaitingTimeInMinutes { get; set; }
+        public int nWaitingPrice { get; set; }
+        public int nTotalRate { get; set; }
+        public string vPickUpPointReachedTime { get; set; }
+        public string vPickUpPointStartTime { get; set; }
+        public string vDestinationReachedEndTime { get; set; }
+        public bool btClosed { get; set; }
+    }
+}
