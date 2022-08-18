b39ed2cf8536880ef7bfb2d72214a8e8bf03845b
Ajit Patri <ajit_patri@hotmail.com>
2022-08-18 13:17:54 +0530

    ok

--- TrackingAPI/Controllers/DriverMasterController.cs
+++ TrackingAPI/Controllers/DriverMasterController.cs
@@ -302,14 +302,14 @@ namespace TrackingAPI.Controllers
             try
             {
                 string query = "DM_sp_ApproveDrivers";
-                DataTable table = new DataTable(); string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon"); int retValue = 0;
+                DataTable table = new DataTable(); string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon"); SqlDataReader myReader;
                 using (SqlConnection myCon = new SqlConnection(sqlDataSource))
                 {
                     myCon.Open(); using (SqlCommand myCommand = new SqlCommand(query, myCon))
                     {
                         myCommand.CommandType = CommandType.StoredProcedure;
                         myCommand.Parameters.AddWithValue("nUserId", CM.nUserId);
-                        retValue = myCommand.ExecuteNonQuery(); myCon.Close();
+                        myReader = myCommand.ExecuteReader(); table.Load(myReader); myReader.Close(); myCon.Close();
 
                         //Send Email and Sms
                         string MailSubject = MessageTemplate.SubDriverApproved;
